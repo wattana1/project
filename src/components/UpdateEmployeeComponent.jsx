69fa105eea282f2eba37af6861e347d7179e774d
wattana1 <wattana.na@rmuti.ac.th>
2022-08-29 20:09:45 +0700

    first commit

---
+++ src/components/UpdateEmployeeComponent.jsx
@@ -0,0 +1,137 @@
+import React, { Component } from 'react'
+import EmployeeService from '../services/EmployeeService';
+
+class UpdateEmployeeComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            id: this.props.match.params.id,
+            name_Emp: '',
+            username: '',
+            password: '',
+            phone: '',
+            address: '',
+            line: '',
+            slip: ''
+        }
+        this.changeName_EmpHandler = this.changeName_EmpHandler.bind(this);
+        this.changeUsernameHandler = this.changeUsernameHandler.bind(this);
+        this.changePasswordHandler = this.changePasswordHandler.bind(this);
+        this.changePhoneHandler = this.changePhoneHandler.bind(this);
+        this.cchangeLineHandler = this.changeLineHandler.bind(this);
+        this.updateEmployee = this.updateEmployee.bind(this);
+    }
+
+    componentDidMount(){
+        EmployeeService.getEmployeeById(this.state.id).then( (res) =>{
+            let employee = res.data;
+            this.setState({
+                name_Emp: employee.name_Emp,
+                username: employee.username,
+                password: employee.password,
+                phone : employee.phone,
+                address: employee.address,
+                line : employee.line,
+                slip: employee.slip
+            });
+        });
+    }
+
+    updateEmployee = (e) => {
+        e.preventDefault();
+        let employee = {name_Emp: this.state.name_Emp,username: this.state.username,password: this.state.password,phone: this.state.phone,address:this.state.address,line: this.state.line,slip:this.state.slip};
+        console.log('employee => ' + JSON.stringify(employee));
+        console.log('id => ' + JSON.stringify(this.state.id));
+        EmployeeService.updateEmployee(employee, this.state.id).then( res => {
+            this.props.history.push('/employees');
+        });
+    }
+    
+    changeName_EmpHandler= (event) => {
+        this.setState({name_Emp: event.target.value});
+    }
+    changeUsernameHandler= (event) => {
+        this.setState({username: event.target.value});
+    }
+   
+    changePasswordHandler= (event) => {
+        this.setState({password: event.target.value});
+    }
+
+    changePhoneHandler= (event) => {
+        this.setState({phone: event.target.value});
+    }
+    changeAddressHandler= (event) => {
+        this.setState({address: event.target.value});
+    }
+    changeLineHandler= (event) => {
+        this.setState({line: event.target.value});
+    }
+    changeSlipHandler= (event) => {
+        this.setState({slip: event.target.value});
+    }
+
+    cancel(){
+        this.props.history.push('/employees');
+    }
+
+    render() {
+        return (
+            <div>
+                <br></br>
+                   <div className = "container">
+                        <div className = "row">
+                            <div className = "card col-md-6 offset-md-3 offset-md-3">
+                                <h3 className="text-center">Update Employee</h3>
+                                <div className = "card-body">
+                                    <form>
+                                    <div className = "form-group">
+                                            <label> Name </label>
+                                            <input placeholder="Name" name="name_Emp" className="form-control" 
+                                                value={this.state.name_Emp} onChange={this.changeName_EmpHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Username </label>
+                                            <input placeholder="Username" name="username" className="form-control" 
+                                                value={this.state.username} onChange={this.changeUsernameHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Password </label>
+                                            <input type='password' placeholder="Password" name="password" className="form-control" 
+                                                value={this.state.password} onChange={this.changePasswordHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Phone </label>
+                                            <input placeholder="Phone" name="phone" className="form-control" 
+                                                value={this.state.phone} onChange={this.changePhoneHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Address </label>
+                                            <input placeholder="Address" name="address" className="form-control" 
+                                                value={this.state.address} onChange={this.changeAddressHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Line </label>
+                                            <input placeholder="Line" name="line" className="form-control" 
+                                                value={this.state.line} onChange={this.changeLineHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Slip </label>
+                                            <input placeholder="Slip" name="slip" className="form-control" 
+                                                value={this.state.slip} onChange={this.changeSlipHandler}/>
+                                        </div>
+                                        <button className="btn btn-success" onClick={this.updateEmployee}>Save</button>
+                                        <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>Cancel</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                   </div>
+            </div>
+        )
+    }
+}
+
+export default UpdateEmployeeComponent
---
+++ src/components/ViewEmployeeComponent.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import EmployeeService from '../services/EmployeeService'
+
+class ViewEmployeeComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            id: this.props.match.params.id,
+            employee: {}
+        }
+    }
+
+    componentDidMount(){
+        EmployeeService.getEmployeeById(this.state.id).then( res => {
+            this.setState({employee: res.data});
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <br></br>
+                <div className = "card col-md-6 offset-md-3">
+                    <h3 className = "text-center"> View Employee Details</h3>
+                    <div className = "card-body">
+                        <div className = "row">
+                            <label> Name:  </label>
+                            <div> {  this.state.employee.name_Emp}</div>
+                        </div>
+                        <div className = "row">
+                            <label> Username:  </label>
+                            <div> {  this.state.employee.username }</div>
+                        </div>
+                        <div className = "row">
+                            <label> Password:  </label>
+                            <div> { this.state.employee.password}</div>
+                        </div>
+                        <div className = "row">
+                            <label> Phone:  </label>
+                            <div> { this.state.employee.phone}</div>
+                        </div>
+                        <div className = "row">
+                            <label> Address:  </label>
+                            <div> {  this.state.employee.address}</div>
+                        </div>
+                        <div className = "row">
+                            <label> Line:  </label>
+                            <div> {  this.state.employee.line}</div>
+                        </div>
+                        <div className = "row">
+                            <label> Slip:  </label>
+                            <div> { this.state.employee.slip}</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ViewEmployeeComponent
